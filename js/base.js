b768dcb480ab0d9555257d3bf62ea7ef6f56cccf
unknown <helbadboy.outsider@gmail.com>
2025-05-27 17:17:46 +0330

    push project to git

---
+++ js/base.js
@@ -0,0 +1,59 @@
+
+$(document).ready(function () {
+
+    $(".items ").niceScroll({
+        railpadding: {top: 0, right: 2, left: 0, bottom: 0},
+        cursorcolor: "#131313",
+        cursoropacitymin: 0.6,
+        cursorwidth: "7px",
+        cursorborder: "none"
+    });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+})
+
+
+$(document).ready(function () {
+
+    $(".chat-items ").niceScroll({
+        railpadding: {top: 0, right: 2, left: 0, bottom: 0},
+        cursorcolor: "#a3a2a2",
+        cursoropacitymin: 0.6,
+        cursorwidth: "7px",
+        cursorborder: "none"
+    });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+})
+
+
+
+
+
+
+
+
